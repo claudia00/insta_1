61281c17d093018269f5f77ccbab1b969d28e309
claudia00 <claudia.martinez91@qmail.cuny.edu>
2019-03-18 00:35:45 -0400

    part 1 instagram_clone, login, log out, fileprovider, query posts, post, camera function works

---
+++ app/src/main/java/com/example/insta_droid1/LoginActivity.java
@@ -0,0 +1,62 @@
+package com.example.insta_droid1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                login(username, password);
+            }
+        });
+    }
+
+
+    private void login(String username, String password) {
+        //TODO: navigate to new activity if the user has signed properly
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+
+                if (user != null) {
+                    Toast.makeText(LoginActivity.this, "Hooray user logged in", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(i);
+                    finish();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Signed in Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+}
+
